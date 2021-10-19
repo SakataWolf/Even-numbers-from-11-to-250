3fb8710708b59ee70ddd9e29608ecc0e3dab32e2
SakataWolf <87884401+SakataWolf@users.noreply.github.com>
2021-10-19 17:45:33 -0300

    Add files via upload

---
+++ Numeros Pares de 11 a 250/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Números Pares entre 11 e 250\n\n");
+            Console.WriteLine("Aperte enter pra continuar");
+            Console.ReadKey();
+
+
+            for (int i = 11; i < 250; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
